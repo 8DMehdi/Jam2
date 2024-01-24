ab003fabedc0b0018a0c5c5956ebd22cf1657154
8DMehdi <111686215+8DMehdi@users.noreply.github.com>
2024-01-24 08:46:37 +0100

    main

--- A mere night/Assets/Mehdi/scripts/cam.cs
+++ A mere night/Assets/Mehdi/scripts/cam.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class cam : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(5f, 5f, -10f);
+    private Vector3 offset = new Vector3(0f, 5f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
